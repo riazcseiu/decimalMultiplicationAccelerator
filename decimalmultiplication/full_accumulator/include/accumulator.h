45d41a4fb98b5842d1ccbde8c328dcbec84b91f3
riaz mian <riaz.cseiu@gmail.com>
2020-02-20 13:09:03 +0900

    full

---
+++ decimalmultiplication/full_accumulator/include/accumulator.h
@@ -0,0 +1,36 @@
+// See LICENSE for license details.
+
+#ifndef SRC_MAIN_C_ACCUMULATOR_H
+#define SRC_MAIN_C_ACCUMULATOR_H
+
+#include "rocc-software/src/xcustom.h"
+
+#define k_DO_WRITE 0
+#define k_DO_READ 1
+#define k_DO_LOAD 2
+#define k_DO_ACCUM 3
+#define k_DO_BCD 4
+#define k_DO_CONVERT 6
+#define k_DO_ACCMUL 7
+
+
+
+#define XCUSTOM_ACC 0
+
+#define doWrite(y, rocc_rd, data)                                       \
+  ROCC_INSTRUCTION(XCUSTOM_ACC, y, data, rocc_rd, k_DO_WRITE);
+#define doRead(y, rocc_rd)                                              \
+  ROCC_INSTRUCTION(XCUSTOM_ACC, y, 0, rocc_rd, k_DO_READ);
+#define doLoad(y, rocc_rd, mem_addr)                                    \
+  ROCC_INSTRUCTION(XCUSTOM_ACC, y, mem_addr, rocc_rd, k_DO_LOAD);
+#define doAccum(y, rocc_rd, data) \
+  ROCC_INSTRUCTION(XCUSTOM_ACC, y, data, rocc_rd, k_DO_ACCUM);
+#define doBCD(y, rocc_rd, data) \
+  ROCC_INSTRUCTION(XCUSTOM_ACC, y, data, rocc_rd, k_DO_BCD);
+#define doConvert(y, rocc_rd, data) \
+  ROCC_INSTRUCTION(XCUSTOM_ACC, y, data, rocc_rd, k_DO_CONVERT);
+#define doAccummul(y, rocc_rd, data) \
+  ROCC_INSTRUCTION(XCUSTOM_ACC, y, data, rocc_rd, k_DO_ACCMUL);
+#
+
+#endif  // SRC_MAIN_C_ACCUMULATOR_H
